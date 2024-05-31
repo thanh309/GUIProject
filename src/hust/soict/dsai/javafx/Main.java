78f5ee22a2cb992e05da1910a098ecb99378cc5d
thanh309 <149485828+thanh309@users.noreply.github.com>
2024-06-01 04:13:22 +0700

    4. finished

--- src/hust/soict/dsai/javafx/Main.java
+++
@@ -1,9 +0,0 @@
-package hust.soict.dsai.javafx;
-
-import javafx.application.Application;
-
-public class Main {
-    public static void main(String[] args) {
-        Application.launch(Painter.class);
-    }
-}
--- src/hust/soict/dsai/javafx/PainterController.java
+++ src/hust/soict/dsai/javafx/PainterController.java
@@ -2,15 +2,27 @@ package hust.soict.dsai.javafx;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 
 public class PainterController {
 
     @FXML
     private Pane drawingAreaPane;
+
+    @FXML
+    private RadioButton eraserRadioButton;
+
+    @FXML
+    private RadioButton penRadioButton;
+
+    @FXML
+    private ToggleGroup toolsToggleGroup;
 
     @FXML
     void clearButtonPressed(ActionEvent event) {
@@ -19,9 +31,16 @@ public class PainterController {
 
     @FXML
     void drawingAreaMouseDragged(MouseEvent event) {
-        Circle newCircle = new Circle(event.getX(),
-                event.getY(), 4, Color.BLACK);
-        drawingAreaPane.getChildren().add(newCircle);
+        RadioButton selectedRadioButton = (RadioButton) toolsToggleGroup.getSelectedToggle();
+        if (selectedRadioButton == penRadioButton) {
+            Circle newCircle = new Circle(event.getX(),
+                    event.getY(), 4, Color.BLACK);
+            drawingAreaPane.getChildren().add(newCircle);
+        } else {
+            Rectangle newRectangle = new Rectangle(20, 20, Color.WHITE);
+            newRectangle.setX(event.getX() - 10);
+            newRectangle.setY(event.getY() - 10);
+            drawingAreaPane.getChildren().add(newRectangle);
+        }
     }
-
 }
