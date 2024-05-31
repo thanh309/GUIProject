60fa788aa41203702bad7614fb42f5934e2a3991
thanh309 <149485828+thanh309@users.noreply.github.com>
2024-06-01 03:36:32 +0700

    4.3 finished

---
+++ src/hust/soict/dsai/javafx/Main.java
@@ -0,0 +1,9 @@
+package hust.soict.dsai.javafx;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(Painter.class);
+    }
+}
